298892d263a5e12fbdd79b8f56a11b1ffcb87f5f
camilazucchi <camilazucchi@outlook.com>
2022-08-06 13:09:45 -0300

    alterando script

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,17 @@ const calcBMI = ( ) => {
         const bmi = (weight.value / (height.value * height.value)).toFixed(2);
         let classification = classificationResult;
 
-        if (bmi >= 0 && bmi < 16.9) {
+        if (bmi >= 0 && bmi <= 16.9) {
             classification = 'Você está muito abaixo do peso.';
         } if (bmi == 17 && bmi <= 18.4) {
             classification = 'Você está abaixo do peso.';
-        } if (bmi >= 18.5 && bmi< 24.9) {
+        } if (bmi >= 18.5 && bmi <= 24.9) {
             classification = 'Você está com o peso normal.';
-        } if (bmi >= 25 && bmi < 29.9) {
+        } if (bmi >= 25 && bmi <= 29.9) {
             classification = 'Você está acima do peso.';
-        } if (bmi >= 30 && bmi < 34.9) {
+        } if (bmi >= 30 && bmi <= 34.9) {
             classification = 'Você está com obesidade grau I.';
-        } if (bmi >= 35 && bmi < 40) {
+        } if (bmi >= 35 && bmi <= 40) {
             classification = 'Você está com obesidade grau II.';
         } if (bmi > 40) {
             classification = 'Você está com obesidade grau III.';
